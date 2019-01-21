34c702da065b276d39f8b159b07f273cc87ea1b7
Waleed Uddin <mwaleed16@hotmail.com>
2019-01-20 17:50:23 -0800

    Update shortList.cpp

--- shortList.cpp
+++ shortList.cpp
@@ -9,7 +9,18 @@ vector<string> shortList(string[], int &size); //sorting function
 
 int main()
 {
+    // int names_size;
+    // cout << "How many names you wish to enter?" << endl;
+    // cin >> names_size;
+    // string names[names_size];
+    // cout << "Input " << names_size << " names(press enter after each entry):" << endl;
+    // for(int i=0; i<names_size; i++){
+    //     cin >> names[i];
+    // }
+    
+    //For using User-Input names, Comment following array and Uncomment code above
     string names[7] = {"alison", "bob", "kate", "son", "carl", "ansen", "mike"}; //Input Array of names
+    
     int size = sizeof(names)/sizeof(names[0]);
     
     printOutput(shortList(names,size));
@@ -23,11 +34,13 @@ vector<string> shortList(string names[], int &size){
      
      
        for(int i=0; i<size; i++){
-       int count = 0;
-       int state = 0;
+       int count = 0;    //local counter for alphabets in each name
+       int state = 0;    //counter decides which names to be included in shortlist
        
-       while(count < names[i].length()){
-          if(std::find(alphabets.begin(), alphabets.end(), names[i][count]) == alphabets.end()) {
+       //scan through alphabets in each name:
+       while(count < names[i].length()){                  
+       //check if current alphabet exists in our alphabets vector, if doen't then add it
+           if(std::find(alphabets.begin(), alphabets.end(), names[i][count]) == alphabets.end()) {
               alphabets.push_back(names[i][count]);
               state++;
             }
@@ -61,5 +74,3 @@ void printOutput(const vector<string> &output){
         cout << output[i] << " ";
     }
 }
-
-
